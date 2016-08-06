8636754413a08d79c171ad9e529d10b314f9f096
Raghu Rana <raghurana@gmail.com>
2016-08-06 17:40:25 +1000

    Cached Fibo added to Dynamic programming.

---
+++ DSA.ConsoleApp/Algorithms/DynamicProgramming/CachedFibonacci.cs
@@ -0,0 +1,19 @@
+﻿namespace DSA.ConsoleApp.Algorithms.DynamicProgramming
+{
+    public static class CachedFibonacci
+    {
+        private static readonly long[] FibCache = new long[200];
+
+        public static long GetFibSum(int n)
+        {
+            if (n <= 1)
+                return 1;
+
+            // Means, no item in the cache previously
+            if (FibCache[n] == 0)
+                FibCache[n] = GetFibSum(n - 1) + GetFibSum(n - 2);
+
+            return FibCache[n];
+        }
+    }
+}
---
+++ DSA.Tests/Algorithms/DynamicProgramming/CachedFibonacciTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using DSA.ConsoleApp.Algorithms.DynamicProgramming;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DSA.Tests.Algorithms.DynamicProgramming
+{
+    [TestClass]
+    public class CachedFibonacciTests
+    {
+        [TestMethod]
+        public void FibTestForLargeValue()
+        {
+            // Without a cached implementation, the complexity would be O(2^N) which is very high for even small values like 80
+            var sum = CachedFibonacci.GetFibSum(100);
+            Assert.AreEqual(1298777728820984005, sum);
+        }
+    }
+}
